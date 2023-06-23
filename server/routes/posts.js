fd0884561d3f448bf6e4596962e816af6e2c0f5c
Artsiom Kirik <kirik.artsiom@mail.ru>
2023-06-24 00:41:05 +0300

    feat: finish posts endpoints for server

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,4 @@
 import express from "express";
-
-
 import {
   getFeedPosts,
   getUserPosts,
@@ -14,7 +12,11 @@ const router = express.Router();
 
 //READ
 router.get("/", verifyToken, getFeedPosts);
-
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 
 //UPDATE
+router.patch("/:id/like", verifyToken, likePost);
+
+
+export default router;
